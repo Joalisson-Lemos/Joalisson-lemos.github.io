8717f40273784bd93ccad132f7736b73c8e777de
Joalisson Lemos <joalissondvd@gmail.com>
2025-09-27 03:00:23 -0300

    Fix: Aplica base path correto (case-sensitive) no deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Joalisson-lemos.github.io/",
+  base: "/Joalisson-Lemos.github.io/",
   plugins: [react(), tailwindcss()],
 })
